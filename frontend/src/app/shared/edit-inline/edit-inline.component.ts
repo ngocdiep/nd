2a0bb5bb09ba28b6fc843255ffe7170a9dfe7578
Diep Nguyen Ngoc <diep.nguyenngoc@dallmeier.com>
2019-01-10 16:10:53 +0700

    make it better

---
+++ frontend/src/app/shared/edit-inline/edit-inline.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-inline',
+  templateUrl: './edit-inline.component.html',
+  styleUrls: ['./edit-inline.component.scss']
+})
+export class EditInlineComponent implements OnInit {
+
+  showSpinner = true;
+  isEditting = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onEsc() {
+
+  }
+
+  onEnter() {
+
+  }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+
+  }
+}
